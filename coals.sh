94b3ffd0be1bfb2adc0c458a830fdf11fc6a7a4e
zoonose <166508313+zoonose@users.noreply.github.com>
2025-09-29 01:39:32 +1300

    0.1.10 changes: - made balance retrieval a bit faster. - made help a bit more helpful. - deprecated 'balance all'.    - 'coals balance' now shows all balances and stakes. - added 'chop' to endless loop. - deprecated 'forever'.    - 'coals [mine|chop|smelt]' now always use loop mode.

--- coals.sh
+++ coals.sh
@@ -1,8 +1,15 @@
 #!/usr/bin/env bash
-coals_version="0.1.9.4"
+coals_version="0.1.10"
 # 'coals': easy launcher for 'coal' (coal-cli 2.9.2)
 
 coal_start() {
+
+   # Set 'coal' parameters
+   # - pay high fee to reprocess for $CHROMIUM and enhance tools because reward is timing dependent
+   freecores=0       # number of CPU cores to leave unused when mining/smelting/chopping
+   buffer_time=2     # seconds
+   prio_smol=111     # lamports
+   prio_big=2112112  # lamports
 
    case "$1" in
       "") coals_help ; exit ;;
@@ -13,73 +20,47 @@ coal_start() {
 
    # Check if 'solana' and 'coal' are installed
    for i in solana coal ; do
-      [ ! "$(which $i)" ] && { echo "Error: $i not installed wyd" ; exit 1 ;}
+      [ ! "$(which $i)" ] && { echo "Error: $i not installed wyd" ; exit ;}
    done
 
    # Auto set a different 'solana' config for each username (or don't)
    case "$USER" in
       # "<your_username_here>") _cfg="--config $HOME/.config/solana/<whatever_config_file_you_want_to_use>.yml" ;;
+      # "asd"?) freecores=2 ;;&
       *) _cfg="--config $HOME/.config/solana/coals_config.yml" ;; # fallback to default
    esac
 
-   # Switch to infinite loop mode if specified
-   [ "$2" == "forever" ] && {
-      case "$1" in
-         "mine") { looptask="mine" ; export -f coal_start ; coals_loop ;} ;;
-         "smelt") { looptask="smelt" ; export -f coal_start ; coals_loop ;} ;;
-         *) { echo "'forever' is only for 'mine' or 'smelt'." ;} ;;
-      esac
-      exit
-   }
+   # Switch to infinite loop mode for work functions
+   shopt -s extglob
+   [ -f "$0" ] && [[ "$1" == @("mine"|"smelt"|"chop") ]] && { looptask="$1" ; export -f coal_start ; coals_loop ; exit ;}
 
-   # Set 'coal' parameters
-   # - pay high fee to reprocess for $CHROMIUM and enhance tools because reward is timing dependent
-   buffer_time=2
-   prio_smol=111
-   prio_big=2112112
-
+   # Parse args
    case "$1" in
-      "mine"|"chop"|"smelt") _params="$1 --cores $(nproc) --buffer-time $buffer_time --priority-fee $prio_smol" ;;
-      "reprocess") _params="$* --priority-fee $prio_big" ;;
-      "inspect"|"unequip"|"craft") _params="$1 --priority-fee $prio_smol" ;;
+      "mine"|"smelt"|"chop")
+         case "$2" in
+            "") _params="$1 --cores $(( $(nproc) - $freecores )) --buffer-time $buffer_time --priority-fee $prio_smol" ;;
+            "forever") { echo "'forever' is default behaviour, no need to specify it" ; exit ;} ;;
+         esac ;;
+      "reprocess") _params="$1 --priority-fee $prio_big" ;;
+      "inspect"|"unequip"|"craft"|"replant") _params="$1 --priority-fee $prio_smol" ;;
       "enhance"|"equip") [ "$2" != "" ] && [ "$2" == "$(grep -oP "[1-9A-HJ-NP-Za-km-z]{32,44}" <<< "$2")" ] &&
          { _params="$1 --tool $2 --priority-fee $( [ "$1" == "equip" ] && echo "$prio_smol" || echo "$prio_big" )" ;} ||
-         { echo "Usage: 'coals $1 <tool_address>'" ; exit 1 ;} ;;
+         { echo "Usage: 'coals $1 <tool_address>'" ; exit ;} ;;
       "stake"|"claim")
          case "$2" in
             "") _params="$1 --priority-fee $prio_smol" ;;
             "coal"|"ingot"|"wood") _params="$1 --resource $2 --priority-fee $prio_smol" ;;
-            "chromium"|"ore") { echo "Nah: $2 can't be staked" ; exit 1 ;} ;;
-            *) { echo "Options: [coal], ingot, wood." ; exit 1 ;} ;;
+            "chromium"|"ore") { echo "Nah: $2 can't be staked" ; exit ;} ;;
+            *) { echo "Options: [coal], ingot, wood." ; exit ;} ;;
          esac ;;
       "balance")
          case "$2" in
-            "") _params="$1" ;;
+            ""|"all") coals_balance ; exit ;;
             "coal"|"ingot"|"wood"|"chromium") _params="$1 --resource $2" ;;
             "ore") { printf 'Balance: %s ORE\n' "$(spl-token balance oreoU2P8bN6jkk3jbaiVxYnG1dCXcYxwhwyK9jSybcp $_cfg)" ; exit ;} ;;
-            "all")
-               printf '\e[1;30m%s\e[m\n' "$(grep -oP "[^/]*$" <<< "$_cfg")"
-               for h in "Balance" "Stake" ; do
-                  printf '\e[1;37m%s\e[m\n' "$h:"
-                  for i in "sol" "coal" "ingot" "wood" "chromium" "ore" ; do
-                     case "$i" in
-                        "sol") [ "$h" == "Balance" ] &&
-                           while read line ; do
-                              [[ "$line" == *"Error"* ]] && { echo Error, check connection ; exit 1 ;} ||
-                              printf '%12.4f %s\n' "$(grep -oP "\d+(\.\d+)?" <<< "$line")" "SOL";
-                           done <<< "$(solana balance $_cfg 2>&1)" ;;
-                        "coal"|"ingot"|"wood")
-                           printf '%12.4f %s\n' "$(coal balance --resource "$i" $_cfg | grep -ioP "(?<=$h:\ )\d+(\.\d+)?")" "${i^^}" ;;
-                        "chromium") [ "$h" == "Balance" ] &&
-                           printf '%12.4f %s\n' "$(coal balance --resource "$i" $_cfg | grep -ioP "(?<=$h:\ )\d+(\.\d+)?")" "${i^^}" ;;
-                        "ore") [ "$h" == "Balance" ] &&
-                           printf '%12.4f %s\n' "$(spl-token balance oreoU2P8bN6jkk3jbaiVxYnG1dCXcYxwhwyK9jSybcp $_cfg)" "${i^^}" ;;
-                     esac
-                  done
-               done ; exit 0 ;;
-            *) { echo "Options: [coal], ingot, wood, chromium, ore, all." ; exit 1 ;} ;;
+            *) { echo "Options: coal, ingot, wood, chromium, ore." ; exit ;} ;;
          esac ;;
-      "version") coal -V ; exit 0 ;;
+      "version") coal -V ; exit ;;
       *) _params="$*" ;;
    esac
 
@@ -92,6 +73,8 @@ coal_start() {
 
    # omg it's happening omg
    bash -c "coal $_cfg $_params"
+
+   # this should only happen if 'coal' freaks out while mining/smelting/chopping
    [[ "$0" != *"coals" ]] && echo "ERROR: Tantrum >:("
 }
 
@@ -99,7 +82,7 @@ coal_start() {
 # Error-catching infinite loop
 coals_loop() {
 
-   # Make ^C exit look a bit cleaner
+   # Cleanup on exit
    trap 'kill $(pidof coal) $_app_pid 2>/dev/null ; [ -f "$_log" ] && rm "$_log" ; sleep 3 ; echo' EXIT
 
    # Terminal command to monitor with 'script'
@@ -125,13 +108,17 @@ coals_loop() {
       script -qfc "$_app" "$_log" &
       _app_pid=$!
 
-      # Kill if death or when log file becomes chonkish
-      tail -F -n +2 "$_log" | while read -r -t 60 -n 15970 line; do
-         [ "$(wc -c < "$_log")" -gt 6942069 ] && kill HUP "$_app_pid" 2>/dev/null && { printf '\n\n%s\n' "Flushing temp file" ; break ;}
-         [ "$(grep -oi "error" <<< "$line")" != "" ] && { for i in {1..5} ; do printf '\U274c ' ; done ; echo ; kill $_app_pid 2>/dev/null ; break ;}
+      # Kill if death or when log file becomes chonkish or if thing-happening stops
+      tail -F -n +2 "$_log" | while read -r -t 30 -n 15970 line; do
+         [ "$(wc -c < "$_log")" -gt 6942069 ] && [[ "$(tail -n 1 "$_log")" == *"OK"* ]] && kill HUP "$_app_pid" 2>/dev/null && { echo ; sleep 3 ; printf '\n\e[1;36m%s\e[m\n\n\n\n' "Flushing temp file" ; break ;}
+         [ "$(grep -oi "error" <<< "$line")" != "" ] && { echo ; for i in {1..5} ; do printf '\U274c ' ; done ; echo ; kill $_app_pid 2>/dev/null ; break ;}
       done
 
-      [ "$(tail "$_log" | grep -P '(error: 0x1)|(foreman)')" != "" ] && { printf '\n%s\n' "RUH ROH This is probably due to insufficient balance. eg., not enough coal to run the smelter!" ; break ;}
+      # Catch (probable) smelter failure and break the loop
+      [ "$(tail "$_log" | grep -P '(error: 0x1)|(foreman)')" != "" ] && { printf '\n%s\n' "RUH ROH Probably not enough coal for the smelter!" ; break ;}
+
+      # Catch ecocide, replant, and break the loop
+      [ "$(tail "$_log" | grep -P '(Needs reset)')" != "" ] && { printf '\n%s\e[48;5;130m\e[38;5;226m%s\e[38;5;21m%s\e[38;5;220m%s\e[m\n%s\n\n' "RUH ROH All the trees have been chopped! Lorax is judging you  " ">" ":" "{ " "Remember to replant so the forest can grow back!" ; break ;}
 
       # Hold horses
       sleep 3
@@ -139,6 +126,37 @@ coals_loop() {
    done
    exit
 }
+
+
+coals_balance() {
+
+   printf '\e[1;30m%s\e[m\n' "$(grep -oP "[^/]*$" <<< "$_cfg")"
+
+   declare -A coals_stakes
+   printf '\e[1;37m%s\e[m\n' "Balance:"
+   for i in "sol" "coal" "ingot" "wood" "chromium" "ore" ; do
+      case "$i" in
+         "sol")
+            while read line ; do
+               [[ "$line" == *"Error"* ]] && { echo Error, check connection ; exit 1 ;} ||
+               printf '%12.4f %s\n' "$(grep -oP "\d+(\.\d+)?" <<< "$line")" "SOL";
+            done <<< "$(solana balance $_cfg 2>&1)" ;;
+         "coal"|"ingot"|"wood"|"chromium")
+            while read line; do
+               [[ "$line" == "Balance"* ]] && printf '%12.4f %s\n' "$(grep -ioP "\d+(\.\d+)?" <<< "$line")" "${i^^}"
+               [[ "$line" == "Stake"* ]] && coals_stakes[$i]="$(grep -ioP "\d+(\.\d+)?" <<< "$line")"
+            done <<< "$(coal balance --resource $i $_cfg)" ;;
+         "ore")
+            printf '%12.4f %s\n' "$(spl-token balance oreoU2P8bN6jkk3jbaiVxYnG1dCXcYxwhwyK9jSybcp $_cfg)" "${i^^}" ;;
+      esac
+   done
+
+   printf '\e[1;37m%s\e[m\n' "Stake:"
+   for i in $(echo "${!coals_stakes[@]}" | tr " " "\n" | sort) ; do
+      printf '%12.4f %s\n' "${coals_stakes[$i]}" "${i^^}"
+   done
+}
+
 
 coals_update() {
    local fetch_temp
@@ -149,9 +167,11 @@ coals_update() {
    [ -f "$fetch_temp" ] && rm "$fetch_temp"
 }
 
+
 coals_install() {
-   echo "Installing coals $coals_version"
-   echo
+
+   echo "Installing coals $coals_version" ; echo
+
    # Check for and remove previous version
    [ -f "$HOME/.local/bin/coals" ] && coals_checkver && { printf '%s' "Removing previous version..." ; rm "$HOME/.local/bin/coals" ;} && echo "done"
 
@@ -162,16 +182,17 @@ coals_install() {
    [ "$(echo "$PATH" | tr ":" "\n" | grep "$HOME/\.local/bin$")" == "" ] && export PATH="$HOME/.local/bin:$PATH"
 
    # Create default config file if it doesn't exist
-   # The commitment level is 'processed' instead of solana's default of 'final' to help with transaction wait times.
+   # The commitment level is 'processed' instead of (default) 'final' to help with transaction wait times.
    local coalfig="$HOME/.config/solana/coals_config.yml"
    [ ! -f "$coalfig" ] && { printf '%s\n%s\n%s\n%s\n%s\n' "---" "json_rpc_url: 'https://api.mainnet-beta.solana.com'" "websocket_url: ''" "keypair_path: '$HOME/.config/solana/id.json'" "commitment: 'processed'" > "$coalfig" ; echo "Created default config file at $coalfig" ;}
 
    # Check that ~/.local/bin exists and move this script there and rename to 'coals' and make it executable and report result
-   [ -d "$HOME/.local/bin" ] && mv "$0" "$HOME/.local/bin/coals" && chmod +x "$HOME/.local/bin/coals" && printf '\n%s\n%s\n\n' "Installed in $HOME/.local/bin" "run 'coals' to see a list of commands" || { echo "Failed to install" ; exit 1 ;}
+   [ -d "$HOME/.local/bin" ] && mv "$0" "$HOME/.local/bin/coals" && chmod +x "$HOME/.local/bin/coals" && printf '%s\n%s\n\n' "Installed in $HOME/.local/bin" "run 'coals' to see a list of commands" || { echo "Failed to install" ; exit 1 ;}
 }
 
 
 coals_checkver() {
+
    # Set delimiter to '.' and make arrays of version numbers
    local IFS=. cver_exist cver_this cver_this_isnewer
    cver_this=($coals_version)
@@ -205,21 +226,35 @@ coals_uninstall() {
 
 
 coals_help() {
-   cat <<< "
-Every 'coals' command
-All other commands (including invalid ones) are passed through directly to 'coal':
+   cat <<< "GMM!
+
+Notes:
+- cost of mining/smelting/chopping is approx $(awk -v var="$prio_smol" 'BEGIN {printf "%.2g", (var+5000)*60/10^9}') sol per hour.
+   (5000 lamport base fee plus $prio_smol lamport priority fee per transaction)
+   (smelting also burns coal and wraps ore (see below))
+
+- cost of reprocessing and enhancing is $(( $prio_big + 5000 )) lamports ($(awk -v var="$prio_big" 'BEGIN {printf "%.2g", (var+5000)/10^9}') sol) per transaction.
+
+- to adjust fees, edit '~/.local/bin/coals' and change ['prio_smol'|'prio_big'] variables near the top.
+- to leave some of the CPU unused while doing work, edit '~/.local/bin/coals' and change 'freecores'.
+
+- to use a different solana keypair, edit '~/.config/solana/coals_config.yml'.
+
+- mining/smelting/chopping will auto-restart on non-fatal errors.
+
+- commands not listed here (including invalid & typos) will be passed through to 'coal'.
+
+Every 'coals' command:
    coals                        # show this help message
    coals help                   # show this help message and the 'coal' help message
    coals mine                   # mine for coal
-   coals mine forever           # mine for coal and auto-restart on error
    coals smelt                  # smelt for iron ingots (cost 75 coal and 0.01 ore per ingot)
-   coals smelt forever          # smelt for iron ingots and auto-restart on error
    coals chop                   # chop for wood
    coals replant                # replant trees after chopping
-   coals reprocess              # reprocess for chromium
+   coals reprocess              # reprocess for chromium (cost $(awk -v var="$prio_big" 'BEGIN {printf "%.2g", (var+5000)/10^9}') sol)
+   coals craft                  # craft a new pickaxe (cost 3 ingot and 2 wood)
    coals inspect                # inspect currently equipped pickaxe
    coals unequip                # unequip currently equipped pickaxe
-   coals craft                  # craft a new pickaxe (cost 3 ingot and 2 wood)
    coals enhance <tool_address> # enhance specified pickaxe (cost 1 chromium and 0.01 sol)
    coals equip <tool_address>   # equip specified pickaxe
    coals stake                  # stake all coal in wallet
@@ -230,13 +265,13 @@ All other commands (including invalid ones) are passed through directly to 'coal
    coals claim coal             # claim all staked coal
    coals claim ingot            # claim all staked ingot
    coals claim wood             # claim all staked wood
-   coals balance                # show coal balance
+   coals balance                # show all balances (sol, coal, ingot, wood, chromium, ore)
+   coals balance all            # show all balances (sol, coal, ingot, wood, chromium, ore)
    coals balance coal           # show coal balance
    coals balance ingot          # show ingot balance
    coals balance wood           # show wood balance
    coals balance chromium       # show chromium balance
    coals balance ore            # show ore balance
-   coals balance all            # show all balances (sol, coal, ingot, wood, chromium, ore)
    coals version                # show version numbers of 'coals' and 'coal'
    coals update                 # update 'coals' to latest version
    coals uninstall              # uninstall 'coals'
@@ -245,10 +280,8 @@ All other commands (including invalid ones) are passed through directly to 'coal
 
 #------------------------------------------------------------------------------
 
-# Run installer if script filename ends with 'coals.sh'
 [ -f "$0" ] || { echo "Not like this" ; exit 1 ;}
 [[ "$0" != "$HOME/.local/bin/coals" ]] && { coals_install ; exit ;}
 
-# Otherwise run the main function
 echo "coals $coals_version"
 coal_start "$@"
